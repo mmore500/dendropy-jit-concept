da450c1321416c98f9536210f4e8ce240b348708
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-10-05 23:55:35 -0400

    Fixup out of bounds index, tweak impl for perf

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from timeit import timeit
+import random
 
 from pylib.treeness import treeness as shim_treeness
 from pylib import shim
@@ -10,25 +11,24 @@ from dendropy.calculate.treemeasure import treeness as dp_treeness
 
 if __name__ == "__main__":
 
-    # Number of taxa (tips) you want in your tree
-    num_taxa = 500
-
-    # Generate a random tree with 5 taxa
-    # taxa = dendropy.TaxonNamespace(['t{0}'.format(i) for i in range(num_taxa)])
-    dp_tree = dp.simulate.treesim.birth_death_tree(
-        birth_rate=1.0,
-        death_rate=0.5,
-        max_time=6.0,
+    # Generate a random tree
+    dp_tree = dp.model.coalescent.pure_kingman_tree_shape(
+        num_leaves=2000,
+        pop_size=2000,
+        rng=random.Random(1),
     )
+    print(f"{len(dp_tree.nodes())=}")
     for node in dp_tree:
         node.edge.length = 1
     dp_result = dp_treeness(dp_tree)
 
-    shim_tree = shim.Tree(
-        apc.RosettaTree(dp_tree).as_alife,
-    )
+    alifestd_df = apc.RosettaTree(dp_tree).as_alife
+    print(f"{len(alifestd_df)=}")
+    shim_tree = shim.Tree(alifestd_df)
     shim_result = shim_treeness(shim_tree)
 
-    print(f"{num_taxa=} {dp_result=} {shim_result=}")
-    print(f"{timeit(lambda: shim_treeness(shim_tree))=}")
-    print(f"{timeit(lambda: dp_treeness(dp_tree))=}")
+    print(f"{dp_result=} {shim_result=}")
+
+    shim_time = timeit(lambda: shim_treeness(shim_tree), number=10**3)
+    dp_time = timeit(lambda: dp_treeness(dp_tree), number=10**3)
+    print(f"{dp_time=} {shim_time=}")
--- pylib/shim.py
+++ pylib/shim.py
@@ -1,13 +1,30 @@
 import typing
 
 from hstrat import _auxiliary_lib as hstrat_aux
+import numba
 from numba import int32 as nb_int32
 from numba.experimental import jitclass
 import numpy as np
 import pandas as pd
 
 
-@jitclass({})
+@jitclass(
+    [
+        ("arr", nb_int32[:]),
+        ("arrs", numba.extending.as_numba_type(typing.Set[nb_int32])),
+    ]
+)
+class Arr:
+    def __init__(self: "Arr", arr: np.array) -> None:
+        self.arr = arr
+        self.arrs = set([v for i, v in enumerate(arr) if i != v])
+
+
+# https://stackoverflow.com/a/38684908/17332200
+arr_type = Arr.class_type.instance_type
+
+
+@jitclass([])
 class Edge:
     def __init__(self: "Edge") -> None:
         pass
@@ -18,19 +35,19 @@ class Edge:
 
 
 @jitclass(
-    {
-        "_ancestor_ids": nb_int32[:],
-        "_pos": nb_int32,
-    },
+    [
+        ("_ancestor_ids", arr_type),
+        ("_pos", nb_int32),
+    ],
 )
 class Node:
 
-    _ancestor_ids: np.array
+    _ancestor_ids: Arr
     _pos: int
 
     def __init__(
         self: "Node",
-        ancestor_ids: np.array,
+        ancestor_ids: Arr,
         pos: int,
     ) -> None:
         self._ancestor_ids = ancestor_ids
@@ -39,7 +56,7 @@ class Node:
     @property
     def _parent_node(self: "Node") -> typing.Optional["Node"]:
         pos = self._pos
-        ancestor_id = self._ancestor_ids[pos]
+        ancestor_id = self._ancestor_ids.arr[pos]
         if ancestor_id == pos:
             return None
         else:
@@ -54,33 +71,37 @@ class Node:
 
     def is_leaf(self: "Node") -> bool:
         pos = self._pos
-        return not pos in self._ancestor_ids
+        return not pos in self._ancestor_ids.arrs
+        # return not pos in self._ancestor_ids.arr[pos + 1 :]
 
 
 @jitclass(
-    {
-        "_ancestor_ids": nb_int32[:],
-        "_pos": nb_int32,
-    },
+    [
+        ("_ancestor_ids", arr_type),
+    ],
 )
 class _Tree:
 
-    _ancestor_ids: np.array
+    _ancestor_ids: Arr
 
     def __init__(
         self: "_Tree",
-        ancestor_ids: pd.DataFrame,
+        ancestor_ids: np.array,
     ) -> None:
-        self._ancestor_ids = ancestor_ids
+        self._ancestor_ids = Arr(ancestor_ids)
 
     def postorder_node_iter(self: "_Tree") -> typing.Iterator[Node]:
-        size = len(self._ancestor_ids)
-        for pos in range(size, 0, -1):
+        size = len(self._ancestor_ids.arr)
+        for pos in range(size - 1, 0, -1):
             yield Node(self._ancestor_ids, pos)
 
 
 def Tree(data: pd.DataFrame) -> _Tree:
-    ancestor_ids = hstrat_aux.alifestd_to_working_format(
-        data,
-    )["ancestor_id"].astype("int32").values
+    ancestor_ids = (
+        hstrat_aux.alifestd_to_working_format(
+            data,
+        )["ancestor_id"]
+        .astype("int32")
+        .to_numpy()
+    )
     return _Tree(ancestor_ids)
